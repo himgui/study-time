1bdcf05c972a5caa74a1ba9f48aa47f3a55aba23
grodrigues.noc@gmail.com <Guilherme>
2023-11-20 20:05:10 -0300

    First script created.

--- python/python-base-linuxtips/day-1/hello.py
+++ python/python-base-linuxtips/day-1/hello.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
-print("Hello, world!")
+
+"""
+Best hello world app.
+"""
+__version__= "0.0.1"
+__author__= "Guilherme"
+__license__= "Unlicensed"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+msg="Palmeiras"
+
+if current_language == "pt_BR":
+    msg = "Olá mundo!"
+elif current_language == "en_US":
+    msg = "This is America"
+print(msg)
