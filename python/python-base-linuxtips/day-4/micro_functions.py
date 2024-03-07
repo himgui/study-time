6a25c13f15f2026ee4c6610096c95b218d43f5aa
grodrigues.noc@gmail.com <Guilherme>
2024-03-06 23:54:18 -0300

    Getting started with python Functions

---
+++ python/python-base-linuxtips/day-4/micro_functions.py
@@ -0,0 +1,41 @@
+"""
+Functions examples:
+f(x) = 5 * (x / 2)
+"""
+
+from math import sqrt
+
+def f(x):
+    result = 5 * (x / 2)
+    return result
+
+print(f(10))
+print(f(10) == 25)
+
+def print_in_lower(text):
+    """ Procedure with no explicit return"""
+    print(text.lower())
+    # return None
+
+print_in_lower("SAO PAULO")
+
+
+def heron(a, b, c):
+    perimeter = a + b + c
+    s = perimeter / 2
+
+
+    area = s * (s - a) * (s - b) * (s - c)
+    return sqrt(area)
+
+triangle_area = heron(3, 4, 5)
+print(triangle_area)
+
+triangles = [
+    (3, 4, 5),
+    (5, 12, 15)
+]
+
+for t in triangles:
+    area = heron(*t)
+    print(f" The triangle area is: {area}")
